d229587a0757a6a15b62dbb459ea4d0024ccf8ca
proj-travel-01 <projtravel01@gmail.com>
2022-07-01 17:34:27 +0530

    deploy

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+
+
+
 const mysql = require('./database.js')
 const express = require('express')
 
@@ -83,5 +95,7 @@ app.get('/get/end_summary/:tripID', (req, res) => {
     res.end();
 });
 
-app.listen(3000);
+server.listen(port,() => {
+    console.log(`Server running at port `+port);
+  });
 
