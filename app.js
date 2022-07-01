a859a7f25f4a6b2af39386cf53fb000ae57637f0
proj-travel-01 <projtravel01@gmail.com>
2022-07-02 01:02:22 +0530

    .

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
-const http = require('http');
 const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
-
-
-
 
 const mysql = require('./database.js')
 const express = require('express')
 
-app = express();
+const app = express();
 
 mysql.connect((err) => {
     if(err) throw err;
 });
 
-app.get('/', (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/json'});
-    console.log("homemeeeeeeeeeeeeeeeee");
-});
+
 //start trip summary
-app.get('/get/start_summary/:vehNum', (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/json'});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/get/start_summary/:vehNum', (req, response) => {
+    //res.writeHead(200, {'Content-Type':'text/json'});
     let vehNum = req.params.vehNum;
     let sql = 'SELECT * FROM bthbnab1ona1jdeoc3nn.start_trip WHERE VEHICLE_NUM = ? ORDER BY START_TIME DESC LIMIT 1';
+    let data ;
     mysql.query(sql, [vehNum], (err, res) => {
         if(err) throw err;
 
-        console.log(JSON.stringify(res));
-        //res.write("queried")
         
-    
+        response.send(res)
+        response.end()
     });
+    //end
     
-    res.end();
 });
 
 //posting start trip
-app.post('/post/starttrip', (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/html'});
+app.post('/post/starttrip', (req, response) => {
+    //res.writeHead(200, {'Content-Type':'text/html'});
     let driverName = req.query.driverName;
     let vehicleNum = req.query.vehicleNum;
     let startLocation = req.query.startLocation;
@@ -54,16 +45,18 @@ app.post('/post/starttrip', (req, res) => {
     let query = 'INSERT INTO bthbnab1ona1jdeoc3nn.start_trip (DRIVER_NAME, VEHICLE_NUM, START_LOCATION, START_TEMP, LOAD_CAPACITY, CURRENT_KM, START_TIME) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)';
     mysql.query(query,[driverName, vehicleNum, startLocation, startTemp, loadCapacity, currentKM],(err, res) => {
         if(err) throw err;
-
+        
         console.log("inserted start trip details")
+        response.send(res);
+        response.end();
     });
 
-    res.end();
+    
 })
 
 //post end trip details
-app.post('/post/endtrip', (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/html'});
+app.post('/post/endtrip', (req, response) => {
+    //res.writeHead(200, {'Content-Type':'text/html'});
     
     let tripID = req.query.tripID;
     let endLocation = req.query.endLocation;
@@ -75,28 +68,32 @@ app.post('/post/endtrip', (req, res) => {
 
     mysql.query(query,[tripID, endLocation, endTemp, endKM],(err, res) => {
         if(err) throw err;
-
+        
         console.log("inserted end trip details")
+        response.send(res);
+        response.end();
     });
 
-    res.end();
+    
 })
 
 //end trip summary
-app.get('/get/end_summary/:tripID', (req, res) => {
-    res.writeHead(200, {'Content-Type':'text/json'});
+app.get('/get/end_summary/:tripID', (req, result) => {
+    //result.writeHead(200, {'Content-Type':'text/json'});
 
     let tripID = req.params.tripID;
     let sql="SELECT ST.TRIP_ID, ST.VEHICLE_NUM, ST.DRIVER_NAME,  ST.START_LOCATION, ET.END_LOCATION, ST.START_TEMP, ET.END_TEMP, ST.LOAD_CAPACITY, ST.CURRENT_KM, ET.END_KM, (ET.END_KM-ST.CURRENT_KM) AS TOTAL_DIST, ST.START_TIME, ET.END_TIME FROM bthbnab1ona1jdeoc3nn.end_trip AS ET JOIN bthbnab1ona1jdeoc3nn.start_trip AS ST ON ST.TRIP_ID=ET.TRIP_ID WHERE ST.TRIP_ID = ?"
-
+    const data="";
     mysql.query(sql,[tripID],(err, res) => {
         if(err) 
             throw err;
 
-        console.log(JSON.stringify(res));     
+        result.send(res);
+        result.end();
+        
     });
+      
     
-    res.end();
 });
 
 
