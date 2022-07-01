0418f19d48b0afe407f410bb2565e99fe7064010
proj-travel-01 <projtravel01@gmail.com>
2022-07-01 18:48:59 +0530

    ,

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
 app.get('/get/start_summary/:vehNum', (req, res) => {
     res.writeHead(200, {'Content-Type':'text/json'});
     let vehNum = req.params.vehNum;
-    let sql = 'SELECT * FROM TRAVEL.START_TRIP WHERE VEHICLE_NUM = ? ORDER BY START_TIME DESC LIMIT 1';
+    let sql = 'SELECT * FROM bthbnab1ona1jdeoc3nn.start_trip WHERE VEHICLE_NUM = ? ORDER BY START_TIME DESC LIMIT 1';
     mysql.query(sql, [vehNum], (err, res) => {
         if(err) throw err;
 
@@ -51,7 +51,7 @@ app.post('/post/starttrip', (req, res) => {
     let currentKM = req.query.currentKM;
 
 
-    let query = 'INSERT INTO TRAVEL.START_TRIP (DRIVER_NAME, VEHICLE_NUM, START_LOCATION, START_TEMP, LOAD_CAPACITY, CURRENT_KM, START_TIME) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)';
+    let query = 'INSERT INTO bthbnab1ona1jdeoc3nn.start_trip (DRIVER_NAME, VEHICLE_NUM, START_LOCATION, START_TEMP, LOAD_CAPACITY, CURRENT_KM, START_TIME) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)';
     mysql.query(query,[driverName, vehicleNum, startLocation, startTemp, loadCapacity, currentKM],(err, res) => {
         if(err) throw err;
 
@@ -71,7 +71,7 @@ app.post('/post/endtrip', (req, res) => {
     let endKM = req.query.endKM;
 
 
-    let query = 'INSERT INTO TRAVEL.END_TRIP (TRIP_ID, END_LOCATION, END_TEMP, END_KM, END_TIME) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)';
+    let query = 'INSERT INTO bthbnab1ona1jdeoc3nn.end_trip (TRIP_ID, END_LOCATION, END_TEMP, END_KM, END_TIME) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)';
 
     mysql.query(query,[tripID, endLocation, endTemp, endKM],(err, res) => {
         if(err) throw err;
@@ -87,7 +87,7 @@ app.get('/get/end_summary/:tripID', (req, res) => {
     res.writeHead(200, {'Content-Type':'text/json'});
 
     let tripID = req.params.tripID;
-    let sql="SELECT ST.TRIP_ID, ST.VEHICLE_NUM, ST.DRIVER_NAME,  ST.START_LOCATION, ET.END_LOCATION, ST.START_TEMP, ET.END_TEMP, ST.LOAD_CAPACITY, ST.CURRENT_KM, ET.END_KM, (ET.END_KM-ST.CURRENT_KM) AS TOTAL_DIST, ST.START_TIME, ET.END_TIME FROM TRAVEL.END_TRIP AS ET JOIN TRAVEL.START_TRIP AS ST ON ST.TRIP_ID=ET.TRIP_ID WHERE ST.TRIP_ID = ?"
+    let sql="SELECT ST.TRIP_ID, ST.VEHICLE_NUM, ST.DRIVER_NAME,  ST.START_LOCATION, ET.END_LOCATION, ST.START_TEMP, ET.END_TEMP, ST.LOAD_CAPACITY, ST.CURRENT_KM, ET.END_KM, (ET.END_KM-ST.CURRENT_KM) AS TOTAL_DIST, ST.START_TIME, ET.END_TIME FROM bthbnab1ona1jdeoc3nn.end_trip AS ET JOIN bthbnab1ona1jdeoc3nn.start_trip AS ST ON ST.TRIP_ID=ET.TRIP_ID WHERE ST.TRIP_ID = ?"
 
     mysql.query(sql,[tripID],(err, res) => {
         if(err) 
--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 module.exports = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'travel',
-    database : 'travel'
+    host : 'bthbnab1ona1jdeoc3nn-mysql.services.clever-cloud.com',
+    user : 'ura86jn2ejfrplnw',
+    password : 'xtg7IMeYbogjWu4kPmOw',
+    database : 'bthbnab1ona1jdeoc3nn'
 });
